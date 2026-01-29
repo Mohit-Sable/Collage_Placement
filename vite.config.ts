3cb6ff5b2626c09ce5a4c278000cc4312425827c
Mohit Sable <mohitsable07@gmail.com>
2026-01-29 19:39:46 +0530

    Refactor vite.config.ts to support different modes

--- vite.config.ts
+++ vite.config.ts
@@ -5,11 +5,18 @@ import path from "path";
 import { componentTagger } from "lovable-tagger";
 
 
-export default defineConfig({
-  plugins: [react()],
+export default defineConfig(({ mode }) => ({
+
   server: {
     allowedHosts: [
       "collage-placement.onrender.com"
     ]
   }
-});
+  },
+  plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+}));
